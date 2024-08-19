ce42cdaa1ed3b50ac6dd723b75b8bcf8af0ab6b8
Nicolas Mariño <nicolasmarinoparra@gmail.com>
2024-08-18 22:16:32 -0500

    delete name column

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_08_16_112223) do
+ActiveRecord::Schema[7.0].define(version: 2024_08_18_024708) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -30,7 +30,6 @@ ActiveRecord::Schema[7.0].define(version: 2024_08_16_112223) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "name"
     t.string "jti", null: false
     t.string "username"
     t.string "confirmation_token"
